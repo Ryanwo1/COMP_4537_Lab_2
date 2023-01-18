7c394db71643201b387b1859e3b5fa79915487d6
Ryan Odribege <rodribege@my.bcit.ca>
2023-01-17 19:03:44 -0800

    change URL

--- Frontend/script.js
+++ Frontend/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		const message = $('#chatbot-input').val();
 		$.ajax({
 			type: 'POST',
-			url: 'http://localhost:3000/chatbot',
+			url: 'https://lab-2-3o8e.onrender.com/',
 			data: {
 				message: message
 			},
