461e95770bc92021971da831c303f4e9e3e9db84
Ryan Odribege <rodribege@my.bcit.ca>
2023-01-17 22:01:34 -0800

    trying different server route

--- Backend/index.js
+++ Backend/index.js
@@ -33,6 +33,27 @@ app.post('/chatbot', (req, res) => {
 	}
 });
 
+app.post('/chatbot', (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	const message = req.body.message;
+	const number = message.match(/\d+/);
+	if (number) {
+		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+			res.json({
+				text: data
+			});
+		}).catch(error => {
+			res.json({
+				text: "Sorry, I couldn't find any information about that number."
+			});
+		});
+	} else {
+		res.json({
+			text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
+		});
+	}
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
