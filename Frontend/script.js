060a1268f187007d1f5863296ef89e3d250eafbf
Ryan Odribege <rodribege@my.bcit.ca>
2023-01-17 22:15:03 -0800

    re copy/paste jquery

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,30 +1,22 @@
-$(document).ready(function() {
-	$('#chatbot-form').submit(function(event) {
-		event.preventDefault();
-		const message = $('#chatbot-input').val();
-		$.ajax({
-			type: 'POST',
-			url: 'https://lab-2-3o8e.onrender.com/chatbot',
-			data: {
-				message: message
-			},
-			success: function(response) {
-				$('#chatbot-response').text(response.text);
-                    let newMessage = $('<div>', {
-                        class: 'message'
-                    }).text(response.text);
-                    let removeButton = $('<button>', {
-                        class: 'remove-button'
-                    }).text('Remove');
-                    newMessage.append(removeButton);
-                    $('#chat-history').append(newMessage);
-			},
-            
-		});
-	});
-});
-
-$(document).on('click', '.remove-button', function() {
-	$(this).parent().remove();
-});
-
+$(document).ready(function () {
+    $('#chatbot-form').submit(function (event) {
+      event.preventDefault();
+      const message = $('#chatbot-input').val();
+      $.ajax({
+        type: 'POST',
+        url: 'https://lab-2-3o8e.onrender.com/chatbot',
+        data: { message: message },
+        success: function (response) {
+          let newMessage = $('<div>', { class: 'message' }).text(response.text);
+          let removeButton = $('<button>', { class: 'remove-button' }).text('Remove');
+          newMessage.append(removeButton);
+          $('#chat-history').append(newMessage);
+        }
+  
+      });
+      $(document).on('click', '.remove-button', function () {
+        $(this).parent().remove();
+      });
+  
+    });
+  });
